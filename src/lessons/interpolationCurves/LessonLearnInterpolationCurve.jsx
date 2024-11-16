07c8f7ae0b09f3e5ceab7d5b4d12e8b0c9a3f778
Weiyan Tao <taoweiyan1998@gmail.com>
2024-11-16 00:32:41 -0800

    Quick update the layout

--- src/lessons/interpolationCurves/LessonLearnInterpolationCurve.jsx
+++ src/lessons/interpolationCurves/LessonLearnInterpolationCurve.jsx
@@ -1,38 +1,16 @@
 import React, { useState, useContext } from 'react';
-
-import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 
-import MainLayout from '../../components/MainLayout';
-import HeaderBarContent from '../../components/HeaderBarContent';
-import HomeButton from '../../components/HomeButton';
+import LessonLearnLayout from '../../lessons/LessonLearnLayout';
 
 function LessonLearnInterpolationCurve() {
-  const homeButton = (
-    <div style={{ position: "absolute", left: 0, bottom: 20 }}>
-      <HomeButton />
-    </div>
-  );
-  const headbarElement = (
-    <HeaderBarContent leftChildren={homeButton}>
-      <Typography variant="h5" component="div">
-        Interpolation Curves
-      </Typography>
-      <div style={{ height: 20 }} />
-      <Typography variant="h3" component="div">
-        - LEARN -
-      </Typography>
-    </HeaderBarContent>
-  );
 
   return (
-    <MainLayout headbarElement={headbarElement}>
-      <Box component="main" sx={{ flexGrow: 1 }}>
-        <Typography variant="h3" component="div">
-          Say HIHIHI!!!
-        </Typography>
-      </Box>
-    </MainLayout>
+    <LessonLearnLayout pageTitle="Interpolation Curves">
+      <Typography variant="h3" component="div">
+        Say HIHIHI!!!
+      </Typography>
+    </LessonLearnLayout>
   );
 }
 
