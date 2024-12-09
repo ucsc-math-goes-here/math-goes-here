aab05826628f4363eb26c73ab599149a0f2dc190
chapachapa <charlesjypark@gmail.com>
2024-12-08 16:51:29 -0800

    added new links

--- src/lessons/dotProduct/Mastery.jsx
+++ src/lessons/dotProduct/Mastery.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import { Box, Button} from '@mui/material';
+import { OpenInNew } from '@mui/icons-material';
 
 import fresnel from'../../assets/dotprod3d/fresnel.png';
 import fresnel2 from'../../assets/dotprod3d/fresnel2.png';
@@ -9,23 +10,50 @@ import grayscale from'../../assets/dotprod3d/grayscale.png';
 
 function QuizDotProduct() {
   return (
-    <Box component="section" sx={{ mb: 4, alignContent: "start", justifyContent: "start" }}>
+    <Box 
+      component="section" 
+      sx={{ 
+        mb: 4,
+        alignContent: "start",
+        justifyContent: "start",
+      }}>
       
       <Box>
         <h1>
           <strong>Dot Product: </strong> Master
         </h1>
-        <p>Try to make something cool!</p>
       </Box>
 
-      <Box>
-        <iframe width="500px" height="400" src="https://scratch.mit.edu/projects/1106303386/embed" title="Scratch Project Embed"></iframe>
+      <Box sx={{
+          display: "grid",
+          gridTemplateColumns: "50% 50%",
+          gap: "2rem"
+        }}
+      >
+        <Box>
+          <iframe width="500px" height="400" src="https://scratch.mit.edu/projects/1106303386/embed" title="Scratch Project Embed"></iframe>
+        </Box>
+        <Box
+          sx={{
+            textAlign: "left"
+          }}
+        >
 
-        <h2>Try Implementing Dot Proudct</h2>
+          <h2 
+            style={{
+              marginTop: 0
+            }}
+          >
+            Try Using Dot Proudct!
+          </h2>
 
-        <p>Fill out the function for "Dot Product" on Scratch</p>
+          <p>
+            Calculate the dot product and the threshold so that <br/>
+            if the Dot Product greater than Threshold, the bat is seen.
+          </p>
 
-        <Button variant="contained" href="https://scratch.mit.edu/projects/1106303386/" target="_blank">Go Now!</Button>
+          <Button variant="contained" href="https://scratch.mit.edu/projects/1107880402/editor/" target="_blank">Try it out on Scratch!&nbsp;<OpenInNew/></Button>
+        </Box>
       </Box>
 
       {/* <Box component="section" sx={{ mt: 4 }}>
